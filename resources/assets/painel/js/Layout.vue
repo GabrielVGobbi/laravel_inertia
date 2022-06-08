41a1e630db28df61cce614e3527d2ebb8c6f7172
gabrielgobbi <icloud.gobbi@gmail.com>
2022-06-07 22:22:05 -0300

    add Layout vue

---
+++ resources/assets/painel/js/Layout.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="mt-4">
+        <InertiaLink href="/" class="btn btn-primary"> Home </InertiaLink>
+        <InertiaLink href="/about" class="btn btn-primary"> About </InertiaLink>
+    </div>
+    
+    <div>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
