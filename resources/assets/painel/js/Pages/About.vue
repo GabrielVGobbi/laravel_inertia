0569afea1565ef8dd48b0d3d4d896d889f2ae18a
gabrielgobbi <icloud.gobbi@gmail.com>
2022-06-07 22:22:26 -0300

    add About vue

---
+++ resources/assets/painel/js/Pages/About.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        About Telephone {{celphone}}
+        About Index {{index}}
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        celphone: String,
+        index: String,
+    }
+}
+</script>
