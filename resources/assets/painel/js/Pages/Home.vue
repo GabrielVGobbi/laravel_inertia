a20586267b986eea8def7b2a55ad993f93ae54c2
gabrielgobbi <icloud.gobbi@gmail.com>
2022-06-07 22:22:14 -0300

    add Home vue

---
+++ resources/assets/painel/js/Pages/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(item, index) in users" :key="index">
+                {{item.name}}
+            </li>
+        </ul>
+    </div>
+
+   
+</template>
+
+<script>
+export default {
+    props: {
+        users: Object
+    }
+}
+</script>
+
+<style>
+    ul {
+        font-size: 40px;
+    }
+</style>
